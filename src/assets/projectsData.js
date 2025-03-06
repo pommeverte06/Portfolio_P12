8fbcd454871df121fda7722afa6be2d8446e1bfa
pommeverte06 <stephaniebp@protonmail.com>
2025-03-06 14:52:24 +0100

    poursuite redaction

--- src/assets/projectsData.js
+++ src/assets/projectsData.js
@@ -1,11 +1,42 @@
 import projet1 from "../assets/ohmyfood.webp";
 import projet2 from "../assets/sophiebluel.webp";
 import projet5 from "../assets/booki.webp";
-
-
-
+import projet7 from "../assets/KASA-home.webp";
+import projet11 from "../assets/argent-bank-home.webp";
 
 const projectsData = [
+  {
+    image: projet11,
+    title: "Argent Bank",
+    shortDescription: "Développement d'une application bancaire avec React, Redux et une gestion sécurisée des utilisateurs. Mise en place de l'authentification et du tableau de bord, intégration des bonnes pratiques Green Code et optimisation des appels API.",
+    fullDescription:
+      "Pour ce projet, j’ai développé une application bancaire en ligne en utilisant React et Redux afin d'assurer une gestion fluide et optimisée de l’état global de l’application. L’un des défis majeurs était la mise en place d’un système d’authentification sécurisé, permettant aux utilisateurs de se connecter, d’accéder à leur tableau de bord et de consulter les informations de leur profil en toute sécurité. L'accès aux données personnelles est conditionné à une authentification réussie, garantissant ainsi une protection stricte des informations sensibles. L'application intègre également un système de gestion des transactions, avec un affichage détaillé des opérations bancaires du mois en cours. Pour assurer une bonne communication entre le front-end et le back-end, j’ai conçu et documenté les endpoints API en suivant la norme Swagger, facilitant ainsi la maintenance et l’évolution de l’architecture. Consciente de l’impact écologique du développement web, j’ai appliqué des bonnes pratiques Green Code, notamment en optimisant les images et en structurant le projet sous forme de composants réutilisables, afin de minimiser le poids des ressources et d’améliorer les performances. Ce projet m’a permis de renforcer mes compétences en gestion d’état avec Redux, en authentification sécurisée, ainsi qu’en modélisation d’API REST avec Swagger, tout en veillant à l’optimisation et à la modularisation du code pour un développement efficace et durable.",
+    github: "https://github.com/tonrepo/projet2",
+    live: "https://monprojet11.com",
+  },
+
+  {
+    image: projet7,
+    title: "KASA",
+    shortDescription:
+      "Développement d’une application de location immobilière avec React et React Router. Mise en place des composants dynamiques, d'un carrousel interactif et gestion des routes.",
+    fullDescription:
+      "Ce projet m’a permis de concevoir une application moderne et dynamique dédiée à la location immobilière, en appliquant les bonnes pratiques du développement front-end. Développée avec React et React Router, l’application offre une navigation fluide et intuitive, mettant en avant des fonctionnalités interactives et une gestion optimisée des données. L’un des défis majeurs a été la mise en place du routage avec React Router, y compris la gestion des erreurs et des redirections (404) pour garantir une navigation cohérente et sécurisée. J’ai structuré l’application autour de composants réutilisables, assurant une meilleure maintenabilité et évolutivité du code. L’expérience utilisateur a été enrichie grâce à plusieurs fonctionnalités dynamiques : -Carrousel interactif permettant d'afficher plusieurs images par logement, avec une gestion fluide des transitions et des événements utilisateur. -Système de collapsibles pour afficher dynamiquement les détails des propriétés, offrant un affichage épuré et optimisé. -Animations CSS intégrées pour améliorer l’interactivité et rendre l’interface plus agréable. L’ensemble du projet a été structuré avec SASS, facilitant la gestion et l’organisation des styles CSS de manière modulaire. Par ailleurs, les données de l’application sont chargées dynamiquement à partir d’un fichier JSON simulant un backend, permettant ainsi de gérer l'affichage dynamique des logements.L’utilisation de Vite a été un atout pour optimiser les performances et accélérer le développement. J’ai également appliqué des principes de Green Code, en veillant à optimiser les images et à structurer le projet de façon efficiente. Ce projet m’a permis d’approfondir mes connaissances en React, en gestion des routes dynamiques et en structuration modulaire du code. J’ai pu améliorer mon approche du développement d’interfaces interactives, tout en optimisant les performances et l’accessibilité de l’application.",
+    github: "https://github.com/pommeverte06/KASA_P7",
+    live: "https://monprojet7.com",
+  },
+
+  {
+    image: projet2,
+    title: "Sophie Bluel",
+    shortDescription:
+      "Développement dynamique du portfolio d’une architecte d’intérieur en JavaScript. Manipulation du DOM, gestion des événements utilisateurs et intégration d’une API pour rendre le site interactif.",
+    fullDescription:
+      "Dans ce projet, j’ai conçu et développé une page web dynamique pour le portfolio d’une architecte d’intérieur, en utilisant JavaScript Vanilla. L’objectif principal était de transformer un site statique en une application interactive, permettant à l’architecte de gérer ses réalisations via une interface administrable. L’un des enjeux majeurs a été la manipulation avancée du DOM, permettant d'afficher dynamiquement les projets et d'optimiser l'interactivité du site. J’ai mis en place une gestion efficace des événements utilisateur, facilitant les interactions telles que : ✔ Mise à jour dynamique du contenu grâce à la modification directe du DOM. ✔ Gestion des événements utilisateurs (clics, soumissions de formulaires). ✔ Développement d’une modale d’upload de médias, permettant l’ajout de nouvelles réalisations en toute simplicité. Le projet a nécessité l’intégration d’une API REST, permettant la récupération et l’envoi de données pour assurer la mise à jour en temps réel des projets affichés sur le site. De plus, une page de connexion sécurisée a été développée pour restreindre l’accès à l’administration du portfolio, assurant ainsi une gestion sécurisée des contenus. Ce projet m’a permis de renforcer mes compétences en JavaScript pur, en approfondissant ma maîtrise du DOM et des interactions dynamiques. J’ai également travaillé sur la communication avec une API, un élément essentiel pour développer des applications web interactives et réactives.",
+    github: "https://github.com/pommeverte06/S0phieBluel_P6",
+    live: "https://monprojet2.com",
+  },
+
   {
     image: projet1,
     title: "Oh my food",
@@ -16,53 +47,17 @@ const projectsData = [
     github: "https://github.com/pommeverte06/ohMyFood_P4",
     live: "https://pommeverte06.github.io/ohMyFood_P4/",
   },
-  {
-    image: projet2,
-    title: "Sophie Bluel",
-    shortDescription: "Développement dynamique du portfolio d’une architecte d’intérieur en JavaScript. Manipulation du DOM, gestion des événements utilisateurs et intégration d’une API pour rendre le site interactif.",
-    fullDescription:
-      "Dans ce projet, j’ai développé une page web dynamique pour le portfolio d’une architecte d’intérieur en JavaScript Vanilla. L’objectif était de transformer un site statique en une application interactive permettant la gestion des travaux de l’architecte. Compétences mises en œuvre:  Manipulation avancée du DOM pour modifier dynamiquement l'affichage des projets. Gestion des événements utilisateurs (clics, soumissions de formulaire). Création d’une page de connexion sécurisée pour l’administration. Développement d’une modale d’upload de médias. Communication avec une API REST pour récupérer et envoyer des données. Ce projet m’a permis de renforcer mes compétences en JavaScript pur, en comprenant l'importance des interactions dynamiques et de l'échange de données avec un serveur. Un pas essentiel pour développer des applications web interactives et réactives.",
-    github: "https://github.com/pommeverte06/S0phieBluel_P6",
-    live: "https://monprojet2.com",
-  },
-  {
-    image: projet1,
-    title: "KASA",
-    shortDescription: "Développement d’une application de location immobilière avec React et React Router. Mise en place des composants dynamiques, d’un carrousel interactif et gestion des routes avec affichage conditionnel des pages.",
-    fullDescription:
-      "Ce projet m’a permis de développer une application de location immobilière moderne et dynamique avec React et React Router, en appliquant les bonnes pratiques du développement front-end. Compétences acquises: Initialisation d’un projet React avec Vite pour une meilleure optimisation des performances.Mise en place du routage avec React Router, y compris la gestion des erreurs et des redirections (404). Création et gestion de composants réutilisables, facilitant la maintenance et l’évolutivité du projet. Développement d’un carrousel dynamique d’images, avec gestion des événements et des transitions. Utilisation des états React pour rendre les composants interactifs et assurer une navigation fluide. Mise en œuvre du collapsible menu pour afficher dynamiquement les détails des propriétés. Application des animations CSS pour améliorer l’expérience utilisateur et renforcer l’esthétique de l’interface.Structuration du projet avec Sass pour une organisation et une gestion efficace du CSS. Gestion de l’affichage dynamique des données à partir d’un fichier JSON simulant une API backend. Ce projet m’a permis d’approfondir mes connaissances en React, d’améliorer ma maîtrise des routes dynamiques, et de concevoir une expérience utilisateur fluide et intuitive. J’ai également travaillé sur l’optimisation des performances et l’organisation modulaire du code, des compétences essentielles pour le développement d’applications web modernes.",
-    github: "https://github.com/pommeverte06/KASA_P7",
-    live: "https://monprojet3.com",
-  },
-  {
-    image: projet2,
-    title: "Argent Bank",
-    shortDescription: "Une application backend avec gestion de données.",
-    fullDescription:
-      "Projet 2 est une plateforme de gestion de données utilisant un backend robuste en Node.js et une base de données MongoDB. Il inclut un système d'authentification et des fonctionnalités avancées.",
-    github: "https://github.com/tonrepo/projet2",
-    live: "https://monprojet4.com",
-  },
 
   {
     image: projet5,
     title: "Booki",
-    shortDescription: "Intégration responsive de la page d’accueil d’une agence de voyage avec HTML & CSS. Respect des maquettes Figma et implémentation des bonnes pratiques de mise en page.",
+    shortDescription:
+      "Intégration responsive de la page d’accueil d’une agence de voyage avec HTML & CSS. Respect des maquettes Figma et implémentation des bonnes pratiques de mise en page.",
     fullDescription:
-      "Ce projet m’a permis d’intégrer la page d’accueil d’une agence de voyage en appliquant et en respectant scrupuleusement les maquettes fournies sur Figma.  Compétences acquises : Maîtrise des fondamentaux du HTML & CSS, avec une structuration claire et optimisée du code. Création d’une mise en page responsive, en assurant un affichage fluide sur desktop, tablette et mobile.Utilisation des Media Queries pour adapter l’interface aux différentes résolutions d’écran. Mise en place de composants réutilisables pour structurer efficacement l’interface utilisateur. Application des bonnes pratiques CSS (flexbox, grid, gestion des marges et espacements). Intégration des polices et icônes avec Google Fonts & Font Awesome pour améliorer l’esthétique du site. Respect des normes W3C pour un code propre et valide. Utilisation des DevTools pour tester et déboguer l’affichage sur différents appareils. Au cours de ce projet, j’ai consolidé ma capacité à créer une interface web moderne et fluide, optimisée pour l’expérience utilisateur et facilement extensible. Ce travail m’a également permis d’améliorer mes compétences en responsive design et en optimisation CSS, essentielles pour tout développeur front-end.",
+      "Ce projet m’a permis d’intégrer la page d’accueil d’une agence de voyage en appliquant et en respectant scrupuleusement les maquettes fournies sur Figma.  Compétences acquises : Maîtrise des fondamentaux du HTML & CSS, avec une structuration claire et optimisée du code. Création d’une mise en page responsive, en assurant un affichage fluide sur desktop, tablette et mobile.Utilisation des Media Queries pour adapter l’interface aux différentes résolutions d’écran. Application des bonnes pratiques CSS (flexbox, grid, gestion des marges et espacements). Intégration des polices et icônes avec Google Fonts & Font Awesome pour améliorer l’esthétique du site. Respect des normes W3C pour un code propre et valide. Utilisation des DevTools pour tester et déboguer l’affichage sur différents appareils. Au cours de ce projet, j’ai consolidé ma capacité à créer une interface web moderne et fluide, optimisée pour l’expérience utilisateur et facilement extensible. Ce travail m’a également permis d’améliorer mes compétences en responsive design et en optimisation CSS, essentielles pour tout développeur front-end.",
     github: "https://github.com/pommeverte06/Projet3_Booki",
     live: "https://pommeverte06.github.io/Projet3_Booki",
   },
-
-
-
-
-
-
-
-
-
-
 ];
 
 export default projectsData;
--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -7,28 +7,9 @@ function ProjectCard({ project, onClick }) {
       <img src={project.image} alt={project.title} className="project-image" />
       <div className="project-content">
         <h3>{project.title}</h3>
-        <p>{project.shortDescription}</p>{" "}
-        <div className="project-icons">
-          {project.github && (
-            <a
-              href={project.github}
-              target="_blank"
-              rel="noopener noreferrer"
-              onClick={(e) => e.stopPropagation()}
-            >
-              <i className="fa-brands fa-github"></i>
-            </a>
-          )}
-          {project.live && (
-            <a
-              href={project.live}
-              target="_blank"
-              rel="noopener noreferrer"
-              onClick={(e) => e.stopPropagation()}
-            >
-              <i className="fa-solid fa-globe"></i>
-            </a>
-          )}
+        <p>{project.shortDescription}</p>
+        <div className="project-link">
+          <span className="en-savoir-plus">En savoir plus</span>
         </div>
       </div>
     </div>
