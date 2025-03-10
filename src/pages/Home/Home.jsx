3831c6183de8cb3c7567c203884b3cf83ce80e68
pommeverte06 <stephaniebp@protonmail.com>
2025-03-10 17:36:11 +0100

    page accueil plus responsive

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,22 +1,32 @@
+
+
+
 import React from "react";
 import "./home.css";
+import profilePic from "../../assets/404.webp"; // Remplace par ta photo
 
 function Home() {
   return (
-      <div className="container">
-        <>
-        
-        <h1>Bienvenue sur mon portfolio</h1>
+    <div className="home-container">
+      <div className="home-content">
+     
+        <h1>Intégratrice web & passionnée du front-end</h1>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat nisi
-          autem distinctio ut veritatis earum quae dolorem? Fuga quisquam
-          aliquid vel! Exercitationem ut possimus magni mollitia neque officiis
-          minima perferendis eligendi placeat sed? Aut dolore fuga inventore
-          cumque consequatur aperiam!
+          Je conçois des interfaces modernes, responsives et accessibles en utilisant HTML, CSS, 
+          JavaScript et React. Mon objectif ? Un site performant, fluide et optimisé pour tous les supports.
         </p>
-        </>
+        <p className="highlight">
+          Pixel-perfect, performance et accessibilité sont au cœur de mon travail !
+        </p>
+        <div className="cta-buttons">
+          <a href="#portfolio" className="btn-primary">Voir mes réalisations</a>
+          <a href="#contact" className="btn-secondary">Me contacter</a>
+        </div>
       </div>
-      
+      <div className="home-image">
+        <img src={profilePic} alt="Stéphanie Paitre - Intégratrice Web" />
+      </div>
+    </div>
   );
 }
 
