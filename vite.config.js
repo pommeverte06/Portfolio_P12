d60f6db5cea2b726886db0fff6a7698ae4296ce5
pommeverte06 <stephaniebp@protonmail.com>
2025-03-14 13:51:12 +0100

    presque la fin

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,6 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
-  server: {
-    historyApiFallback: true
-  }
-});
+})
