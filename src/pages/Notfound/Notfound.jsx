73e5ac28632d2ba8f3bfe84c50a0f201adc4801d
pommeverte06 <stephaniebp@protonmail.com>
2025-03-15 16:27:18 +0100

    ajout arias

--- src/pages/Notfound/Notfound.jsx
+++ src/pages/Notfound/Notfound.jsx
@@ -6,25 +6,39 @@ import notFoundImage from "../../assets/404.webp";
 function NotFound() {
   return (
     <div className="not-found-container">
-      <img src={notFoundImage} alt="Erreur 404" className="not-found-image" />
+      <img
+        src={notFoundImage}
+        alt="Illustration d'un petit robot tenant un panneau indiquant Erreur 404"
+        className="not-found-image"
+      />
 
-      <div className="not-found-content">
-        <h2>Oups, cette page est introuvable ! </h2>
+      <div className="not-found-content" role="alert">
+        <h1>Oups, cette page est introuvable !</h1>
         <p>
           Il semblerait que vous soyez perdu dans l’espace du web... Pas de
           panique, mon petit robot explorateur est là pour vous guider !
         </p>
+
         <div className="not-found-links">
-          <Link to="/" className="btn-404">
-            {" "}
+          <Link
+            to="/"
+            className="btn-404"
+            aria-label="Retourner à la page d'accueil"
+          >
             Retour à l’Accueil
           </Link>
-          <Link to="/portfolio" className="btn-404">
-            {" "}
+          <Link
+            to="/portfolio"
+            className="btn-404"
+            aria-label="Voir mon portfolio"
+          >
             Voir mes Réalisations
           </Link>
-          <Link to="/contact" className="btn-404">
-            {" "}
+          <Link
+            to="/contact"
+            className="btn-404"
+            aria-label="Me contacter directement"
+          >
             Me contacter
           </Link>
         </div>
