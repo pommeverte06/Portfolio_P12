72a00dd45be1ecdc27f4cb9b4ee1e5a8833e6ddf
pommeverte06 <stephaniebp@protonmail.com>
2025-03-15 13:37:51 +0100

    404 ok

--- src/pages/Notfound/Notfound.jsx
+++ src/pages/Notfound/Notfound.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./notfound.css";
 import notFoundImage from "../../assets/404.webp";
 
@@ -6,6 +7,28 @@ function NotFound() {
   return (
     <div className="not-found-container">
       <img src={notFoundImage} alt="Erreur 404" className="not-found-image" />
+
+      <div className="not-found-content">
+        <h2>Oups, cette page est introuvable ! </h2>
+        <p>
+          Il semblerait que vous soyez perdu dans l’espace du web... Pas de
+          panique, mon petit robot explorateur est là pour vous guider !
+        </p>
+        <div className="not-found-links">
+          <Link to="/" className="btn-404">
+            {" "}
+            Retour à l’Accueil
+          </Link>
+          <Link to="/portfolio" className="btn-404">
+            {" "}
+            Voir mes Réalisations
+          </Link>
+          <Link to="/contact" className="btn-404">
+            {" "}
+            Me contacter
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -31,9 +31,3 @@ export default defineConfig({
 
 
 
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// export default defineConfig({
-//   plugins: [react()],
-// })
